7321938cc4b8181e8a2794f2d63efba2f96e362c
Eric Heinzman <ehu9079@ehu9079-VirtualBox.(none)>
2015-02-26 19:41:18 -0500

    added dataset that includes highest points in the midwest

---
+++ mymountains.js
@@ -0,0 +1,12 @@
+var MYMOUNTAINS = [
+  {name: "Hoosier Hill", height: 1257, state: "Indiana"},
+  {name: "Black Mountain", height: 4145, state: "Kentucky"},
+  {name: "Charles", height: 1235, state: "Illinois"},
+  {name: "Hawkeye Point", height: 1670, state: "Iowa"},
+  {name: "Taum Sauk Mountain", height: 1772, state: "Missouri"},
+  {name: "Ohio", height: 1550, state: "Ohio"},
+  {name: "Mount Arvon", height: 1979, state: "Michigan"}
+];
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = MYMOUNTAINS;
