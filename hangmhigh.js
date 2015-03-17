6dee87c0c856ae197e90caf68e93874e710c3376
Eric Heinzman <ehu9079@ehu9079-VirtualBox.(none)>
2015-03-16 21:03:35 -0400

    dataset which includes 8 of the most notorious outlaws of the west

---
+++ hangmhigh.js
@@ -0,0 +1,17 @@
+OUTLAWS = [
+  {name: "John Wesley Hardin", Nickname: "Little Arkansas", COD: "Shot",Died: "May 26, 1853", Age_Of_Death:42 },
+  {name: "Tom Horn", Nickname: "Tom Hale", COD: "Hanging",Died: "November 20, 1903",Age_Of_Death: 42 }, 
+  {name: "James Brown Miller", Nickname: "Killer Miller", COD: "Hanging",Died: "April 19, 1909", Age_Of_Death: 42 }, 
+ {name: "James Butler Hickok", Nickname: "Wild Bill Hickok", COD: "Murdered",Died: "August 2, 1876", Age_Of_Death: 39 }, 
+ {name: "William Henry McCarty", Nickname: "Billy The Kid", COD: "Shot",Died: "July 14, 1881", Age_Of_Death: 21  }, 
+ {name: "Boone Helm", Nickname: "The Kentucky Cannibal", COD: "Hanging",Died: "January 14, 1864", Age_Of_Death: 35 }, 
+ {name: "Harvey Logan", Nickname: "Kid  Curry", COD: "Shot",Died: "June 17, 1904", Age_Of_Death: 37 }, 
+ {name: "Butch Cassidy", Nickname: "Santiago Maxwell", COD: "Shot",Died: "November 7, 1908", Age_Of_Death: 42 },
+
+];
+
+
+if (typeof module != "undefined" && module.exports)
+  module.exports = OUTLAWS;
+
+
